965d2721517121f3fd1291fde8875c75b5bb311e
Yutsss <adyuta123@gmail.com>
2025-01-29 01:47:09 +0700

    chore: change client url dev

--- src/constants/client-url-constants.ts
+++ src/constants/client-url-constants.ts
@@ -1,5 +1,5 @@
 export enum ClientUrl {
   Local = 'http://localhost:3000',
-  Development = 'https://fe-design-simplified.vercel.app',
-  Production = 'https://designsimplified.co.za',
+  Development = 'https://fe-designsimplified.vercel.app/',
+  Production = '',
 }
